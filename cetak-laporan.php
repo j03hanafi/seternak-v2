bddd9bca4c6b8fc18d7f7c2b83106f39def1b23d
Iqbalmubarak <48642060+Iqbalmubarak@users.noreply.github.com>
2022-10-12 11:02:43 +0700

    Update cetak-laporan.php

--- cetak-laporan.php
+++ cetak-laporan.php
@@ -24,17 +24,105 @@
     $mpdf = new \Mpdf\Mpdf();
 
     $html = '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    haloo
-</body>
-</html>';
+                <html lang="en">
+
+                <head>
+                    <meta charset="utf-8">
+                    <title>Laporan Penjualan Peternak</title>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.4.1/paper.css">
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.4.1/paper.css">
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+                        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                    <style>
+                        @media print {
+                            @page {
+                                size: landscape
+                            }
+                        }
+
+                        @page {
+                            size: A4 landscape;
+                        }
+
+
+                        h1 {
+                            font-weight: bold;
+                            font-size: 20pt;
+                            text-align: center;
+                        }
+
+                        table {
+                            border-collapse: collapse;
+                            width: 100%;
+                        }
+
+                        .table th {
+                            padding: 8px 8px;
+                            border: 1px solid #000000;
+                            font-weight: bold;
+                            text-align: center;
+                            color: white;
+                            background-color: #0e8550;
+                        }
+
+                        .table td {
+                            padding: 3px 3px;
+                            border: 1px solid #000000;
+                            text-align: center;
+                            background-color: white;
+                        }
+
+                        .text-center {
+                            text-align: center;
+                        }
+                    </style>
+                    <style type="text/css" media="print">
+                        .page {
+                            -webkit-transform: rotate(-90deg);
+                            -moz-transform: rotate(-90deg);
+                            filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
+                        }
+                    </style>
+                </head>
+
+                <body>
+                    <section class="sheet padding-10mm">
+                        <div class="row">
+                            <div class="col-12">
+                            </div>
+                            <div class="col-12">
+                                <center>
+                                    <h4><b>Laporan Penjualan Peternak</b></h4>
+                                </center>
+                            </div>
+                        </div>
+                        <br>
+                        <hr>
+                        <div class="row">
+                            <div class="col-12">
+                                <p><strong>Laporan Penjualan pada : '. date_format($date_start,"d M Y") .' s/d '. date_format($date_end,"d M Y") .'</strong></p>
+                            </div>
+                        </div>
+                        
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Produk</th>
+                                    <th>Mitra</th>
+                                    <th>Tanggal</th>
+                                    <th>Kuantitas</th>
+                                    <th>Harga</th>
+                                </tr>
+                            </thead>
+                            
+                        </table>
+
+                        <hr>
+                    </section>
+                </body>
+
+                </html>';
 
     $mpdf->WriteHTML($html);
     $mpdf->Output('Laporan penjualan.pdf', "I");
